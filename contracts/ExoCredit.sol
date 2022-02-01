bb6fafa3f557f48febc51c9acfa920a387bf9154
Ibai-NexxyoLabs <ibai.castanon@nexxyolabs.com>
2022-02-01 17:00:37 +0100

    Created Exo with Proxy

---
+++ contracts/ExoCredit.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+contract ExoCredit is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, AccessControlUpgradeable {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() initializer {}
+
+    function initialize() initializer public {
+        __ERC20_init("ExoCredit", "EXO");
+        __ERC20Burnable_init();
+        __AccessControl_init();
+
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
+    }
+
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
+    }
+}
---
+++ deploy/01_deploy_exo.js
@@ -0,0 +1,25 @@
+module.exports = async ({ getNamedAccounts, deployments }) => {
+
+    const { deploy } = deployments;
+    const { deployer } = await getNamedAccounts();
+
+    const exoToken = await deploy('ExoCredit', {
+        from: deployer,
+        args: [],
+        log: true,
+        proxy: {
+            proxyContract: 'OpenZeppelinTransparentProxy',
+        }
+    });
+
+    console.log('EXO Token deployed at: ', exoToken.address);
+
+    const exoImplementation = await hre.deployments.get('ExoCredit_Implementation');
+    const ExoDeployed = await ethers.getContractAt('ExoCredit', exoImplementation.address);
+    await run("verify:verify", {
+        address: ExoDeployed.address,
+        contract: "contracts/ExoCredit.sol:ExoCredit"
+    });
+};
+
+module.exports.tags = ['ExoCredit'];
