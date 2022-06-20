23b49ee616ebb3fe1c622764dad6014100b08508
Ibai-NexxyoLabs <ibai.castanon@nexxyolabs.com>
2022-06-20 11:21:38 +0200

    New resources deployed

--- deploy/02_deploy_resources.ts
+++ deploy/02_deploy_resources.ts
@@ -3,47 +3,49 @@ import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
 function delay(ms: number) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-const name: string = "Hydrogen";
-const symbol: string = "HDR";
-const multiSigWallet: string = "0x2aBcbdF5a10082F311D666EC58aD1C90948a2F4a";
+const name: string = "Cobalt";
+const symbol: string = "COB";
+const multiSigWallet: string = "0x29e05FADE91a33A413e39D980D86F253123C5fa7";
 
-const deploy: DeployFunction = async function (
-    hre: HardhatRuntimeEnvironment,
-) {
-    const { deployments, getNamedAccounts } = hre;
-    const { deployer } = await getNamedAccounts();
-    const { deploy } = deployments;
+const deploy: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts } = hre;
+  const { deployer } = await getNamedAccounts();
+  const { deploy } = deployments;
 
-    const resourceToken = await deploy('Resource', {
-        from: deployer,
-        args: [],
-        log: true,
-        proxy: {
-            proxyContract: 'OpenZeppelinTransparentProxy',
-            execute: {
-                init: {
-                    methodName: "initialize",
-                    args: [name, symbol, multiSigWallet]
-                }
-            }
-        }
-    });
+  const resourceToken = await deploy("Resource", {
+    from: deployer,
+    args: [],
+    log: true,
+    proxy: {
+      proxyContract: "OpenZeppelinTransparentProxy",
+      execute: {
+        init: {
+          methodName: "initialize",
+          args: [name, symbol, multiSigWallet],
+        },
+      },
+    },
+  });
 
-    console.log('Resource Token  deployed at: ', resourceToken.address);
+  console.log("Resource Token  deployed at: ", resourceToken.address);
 
-    delay(5000);
+  delay(5000);
 
-    const resourceImplementation = await hre.deployments.get('Resource_Implementation');
-    const resourceDeployed = await ethers.getContractAt('Resource', resourceImplementation.address);
-    await hre.run("verify:verify", {
-        address: resourceDeployed.address,
-        contract: "contracts/Resource.sol:Resource"
-    });
-
+  const resourceImplementation = await hre.deployments.get(
+    "Resource_Implementation"
+  );
+  const resourceDeployed = await ethers.getContractAt(
+    "Resource",
+    resourceImplementation.address
+  );
+  await hre.run("verify:verify", {
+    address: resourceDeployed.address,
+    contract: "contracts/Resource.sol:Resource",
+  });
 };
 
-deploy.tags = ['Resource']
+deploy.tags = ["Resource"];
 export default deploy;
